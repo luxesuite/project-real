09b10e113e815dbd7ba8f398a5c12750a7cfd495
Adebest <vaccemane@gmail.com>
2025-06-22 11:46:58 +0100

    slides

--- src/components/UserSlide.tsx
+++ src/components/UserSlide.tsx
@@ -37,11 +37,12 @@ export default function UserSlider() {
       gsap.to(banner, {
         x: 0,
         duration: 0.5,
+        delay:"5s",
         ease: 'power2.out',
         onStart: () => {
           if (banner) {
             banner.innerHTML = `
-              <div class="bg-white text-primary p-2 rounded-r-lg shadow-sm min-w-64 flex items-center gap-x-2 justify-center">
+              <div class="bg-white text-primary p-2 rounded-r-lg shadow-sm min-w-48 flex items-center gap-x-2 justify-center text-[0.8rem]">
                 <svg class="w-4 h-4 stroke-primary fill-none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path d="M12 22c1.1 0 2-.9 2-2h-4c0 1.1.9 2 2 2zm6-6v-5c0-3.07-1.63-5.64-4.5-6.32V4c0-.83-.67-1.5-1.5-1.5s-1.5.67-1.5 1.5v.68C7.64 5.36 6 7.93 6 11v5l-2 2v1h16v-1l-2-2z"/>
                 </svg>
@@ -57,8 +58,8 @@ export default function UserSlider() {
       // Slide out after 2 seconds (stays visible for 1.5s after animation)
       gsap.to(banner, {
         x: -300,
-        duration: 0.5,
-        delay: 1.5,
+        duration: 1,
+        delay: 5,
         ease: 'power2.in',
         onComplete: () => {
           currentIndex.current = (currentIndex.current + 1) % users.length;
@@ -77,7 +78,7 @@ export default function UserSlider() {
   return (
     <div 
       ref={bannerRef} 
-      className="fixed top-[20%] left-0 transform -translate-y-1/2 z-50"
+      className="fixed top-[15%] left-0 transform -translate-y-1/2 z-50"
     />
   );
 }
