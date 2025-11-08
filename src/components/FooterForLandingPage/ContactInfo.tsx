c4c7121557087e15d88121849ed7742676a965d7
Adebest <vaccemane@gmail.com>
2025-11-08 07:40:58 +0100

    fixed

--- src/components/FooterForLandingPage/ContactInfo.tsx
+++ src/components/FooterForLandingPage/ContactInfo.tsx
@@ -11,13 +11,14 @@ const ContactInfo = () => {
       <section className="my-4">
         {/* Contact */}
         <div className="flex gap-x-4 mb-4 hover:text-footerTextOne cursor-pointer">
-            <FaLocationDot size={30} className="text-primary"/> <span>C/O 32 Castlewood Road, London, United Kingdom, N16 6DW</span></div>
+            <FaLocationDot size={25} className="text-primary"/> <span>C/O 32 Castlewood Road, London, United Kingdom, N16 6DW</span></div>
             {/* Mail */}
-        <div className="flex gap-x-4 my-4 hover:text-footerTextOne cursor-pointer">
-            <IoMailUnreadSharp size={30}  className="text-primary"/> <span>realvistaglobalwealth@gmail.com</span></div>
+        <div className="flex items-center gap-x-4 my-4 hover:text-footerTextOne cursor-pointer">
+            <IoMailUnreadSharp size={25}  className="text-primary"/> <span className="text-[0.8rem]">realvistaglobalwealth
+              <br />@gmail.com</span></div>
             {/* Vip */}
         <div className="flex gap-x-4 my-4 hover:text-footerTextOne cursor-pointer">
-            <IoCall  size={30}  className="text-primary" /> <span>+1 (313) 697-8902</span></div>
+            <IoCall  size={25}  className="text-primary" /> <span>+1 (313) 697-8902</span></div>
       </section>
     </div>
   )
